854a2a4280c9fa50bd0f0b96eeb2cda0877eb3db
Wibben <bingr99@hotmail.com>
2019-07-04 12:35:05 -0400

    Implemented hardware, button presses are now pull down (active-LOW), removed debouncing as it was unnecessary for this application

--- Escape Room/Morse_Code/Morse_Code.ino
+++ Escape Room/Morse_Code/Morse_Code.ino
@@ -5,29 +5,28 @@ int numPins = 8; //number of pins to make array iteration easier
 
 //Notes/Tones constants
 //char notes[] = "cdefgabC "; //notes available on the piano
-int MorseUnit = 300; // Number of milliseconds for one unit
+int MorseUnit = 200; // Number of milliseconds for one unit
 int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 }; //frequencies that correspond with the notes
 char *alphabet[26] = { // Holds the morse code for the alphabet (A-Z)
-  
+  "", "", "", "", ".", 
+  "", "", "", "", "", 
+  "", "", "", "", "", 
+  "", "", "", "", "", 
+  "", "", "", "", "", ""
 };
 char *numeral[10] = { // Holds the morse code for the numberals (0-9)
   "-----", ".----", "..---",
   "...--", "....-", ".....",
-  "-....", "--...", "---..", "----.",
+  "-....", "--...", "---..", "----."
 };
 
-//Debounce Variables
-int prevStates[] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW}; //stores the previous state of the pins
-long lastDebounceTime[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //stores the time a particular button was pressed
-int debounceDelay = 50; //debounce time, make it higher if flickers, lower if not sensitive enough
-
 // Messages to play
-char *message[5] = {
-  "ONE",
-  "TWO",
-  "THREE",
-  "FOUR",
-  "FIVE"
+String message[5] = {
+  "E",
+  "E",
+  "E",
+  "E",
+  "E"
 };
 
 //Serial Output
@@ -47,14 +46,15 @@ void playTone(int tone, int duration)
 // Plays the morse for a character
 void playMorse(char character)
 {
+  Serial.println(character);
   // Figure out if it's a letter or number and play from the right array
-  char *morse;
+  String morse;
   if(character>='A' && character<='Z') {
     morse = alphabet[character-'A'];
-  } else morse = numeral[character='0'];
+  } else morse = numeral[character-'0'];
 
   for(int i=0; i<morse.length(); i++) {
-    if(morse[i]=='.') playTone(tones[4],MorseUnit);
+    if(morse[i]=='.') playTone(tones[3],MorseUnit);
     else playTone(tones[4],3*MorseUnit);
     delay(MorseUnit); // Wait a unit before playing next tone
   }
@@ -85,21 +85,6 @@ void disp(String msg)
   if (serialOn) Serial.println(msg);
 }
 
-//reads button with debounce
-int readBtn(int pin) 
-{ 
-  int val = digitalRead(pin); //reads input from pin
-//  Serial.print(val);
-//  Serial.print(" ");
-//  Serial.println(prevStates[pin]);
-  //delay(50);
-  if (val != prevStates[pin] && (millis() - lastDebounceTime[pin]) > debounceDelay) {
-    lastDebounceTime[pin] = millis(); //stores the last time the button was pressed.
-    if(pin==recordPin || pin==playPin) prevStates[pin] = val; //if the button was held long enough, return high
-    return !prevStates[pin];
-  } else return LOW;
-} 
-
 void setup() 
 {
   //initialize pins
@@ -113,8 +98,10 @@ void setup()
 void loop() 
 {
   //Play recorded message based on button pressed
-  for (int i = 0 ; i < 8 ; i++) {
-    if (readBtn(keyPins[i]) == HIGH) { //if the current pin button is being pressed
+  for (int i = 0 ; i < 5; i++) {
+    if(digitalRead(keyPins[i])==LOW){
+    //if (readBtn(keyPins[i]) == HIGH) { //if the current pin button is being pressed
+      Serial.println(keyPins[i]);
       playMessage(i);
     }
   }
